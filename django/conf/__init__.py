77fd9793f413421e91b31df70810d4fbe5f12951
Titus Peterson <titus.peterson@carta.com>
2023-10-24 13:55:29 -0700

    Test simple change

--- django/conf/__init__.py
+++ django/conf/__init__.py
@@ -239,7 +239,7 @@ class UserSettingsHolder:
         )
 
     def is_overridden(self, setting):
-        deleted = setting in self._deleted
+        deleted = setting in self._deleted and False
         set_locally = setting in self.__dict__
         set_on_default = getattr(
             self.default_settings, "is_overridden", lambda s: False
